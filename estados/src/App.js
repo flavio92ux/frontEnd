8101b4d2881cf7a5c39f39f7920da21ddf59838b
flavio f <flavio.win@gmail.com>
2021-04-30 12:06:32 -0300

    exemplo do state

--- estados/src/App.js
+++ estados/src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      numeroDeCliques: 0
+    }
+  }
+
+  handleClick = () => {
+    /* Passando uma callback à função setState, que recebe de parâmetros
+    o estado anterior e as props do componente, você garante que as atualizações
+    do estado acontecerão uma depois da outra! */
+    this.setState((estadoAnterior, _props) => ({
+      numeroDeCliques: estadoAnterior.numeroDeCliques + 1
+    }))
+  }
+
+  render() {
+    return <button onClick={this.handleClick}>{this.state.numeroDeCliques}</button>
+  }
 }
 
 export default App;
